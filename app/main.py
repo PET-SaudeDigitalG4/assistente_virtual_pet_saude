cfd32f8bec3af136c2315e933ad7ec0614a90191
Elis Vieira Weiss <elisvieiraweiss@outlook.com>
2026-01-08 11:48:13 -0300

    feat: implement recursive text splitter with overlap for document chunking

--- app/main.py
+++ app/main.py
@@ -1,19 +1,17 @@
+from app.adapters.splitter import split_docs
 from app.core.ingestion import load_servicos
 from app.adapters.embeddings import get_embeddings
 from app.adapters.vector_store import create_vector_store, create_retriever
 
 def setup_system():
-    # Carrega apenas os documentos da pasta "servicos"
     docs_servicos = load_servicos()
+    
+    chunks_servicos = split_docs(docs_servicos)
 
-    # Cria o modelo de embeddings (HuggingFace, conforme definimos antes)
     embeddings = get_embeddings()
 
-    # Cria o vector store FAISS
-    vector_store = create_vector_store(docs_servicos, embeddings)
+    vector_store = create_vector_store(chunks_servicos, embeddings)
 
-    # Cria o retriever
     retriever = create_retriever(vector_store)
 
-    # sistema usa apenas 1 retriever
     return {"servicos": retriever}
